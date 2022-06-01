c172d80d0c5c3508e9d61c26570280f3186b4c74
NikolayPasyuk <kolyan-1812@mail.ru>
2022-06-01 21:25:33 +0300

    Given a task, what happens when a function is called

--- src/lessons/lesson8/questions.js
+++ src/lessons/lesson8/questions.js
@@ -1,4 +1,5 @@
 //Дана задача,что будет выведено  в консоль
+/*
 const square = {
     side: 5,
     area() {
@@ -8,4 +9,21 @@ const square = {
 };
 
 console.log(square.area())
-console.log(square.perimeter())
+console.log(square.perimeter())*/
+
+//Дана задача,что пройзойдет при вызове функции
+var a = 15;
+
+function foo() {
+    var a = 25;
+
+    var result = function () {
+        console.log(a)
+    }
+    result.a = 35;
+
+    return result
+}
+
+var bar = foo().bind({a: 45})
+bar()
