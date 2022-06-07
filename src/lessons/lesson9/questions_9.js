b917a108e8b63572b07ed25e11122fa7e4568924
NikolayPasyuk <kolyan-1812@mail.ru>
2022-06-07 20:14:39 +0300

    Task 3

--- src/lessons/lesson9/questions_9.js
+++ src/lessons/lesson9/questions_9.js
@@ -13,6 +13,7 @@
 group.showUsersWithGroup()*/
 
 //Task 2
+/*
 function a(x, y) {
     return x * y
 }
@@ -23,4 +24,31 @@ console.log(
 )
 console.log(
     b(3)
+)*/
+//Task 3
+const callCount = (fn) => {
+    let count = 0;
+
+    return [
+        (...args) => {
+            count += 1
+            fn(...args)
+        },
+        () => {
+            return count
+        }
+    ]
+}
+
+const sum = (a, b) => {
+    return a + b
+}
+
+const [addCount, getCount] = callCount(sum)
+
+addCount(1, 2)
+addCount(1, 3)
+addCount(2, 5)
+console.log(
+    getCount()
 )
