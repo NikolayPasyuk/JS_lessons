d7677c388775df19d5d769be2cfd0cb5d0aa9eaa
NikolayPasyuk <kolyan-1812@mail.ru>
2022-06-08 19:29:06 +0300

    Task 12

--- src/lessons/lesson9/questions_9.1.js
+++ src/lessons/lesson9/questions_9.1.js
@@ -1,4 +1,10 @@
 //Task 11
+/*
 console.log('1' + true)
 console.log([1] + true)
-console.log('2' * 3 + true)
+console.log('2' * 3 + true)*/
+
+//Task 12
+console.log(null == undefined)
+console.log(null == null)
+console.log({} + {})
