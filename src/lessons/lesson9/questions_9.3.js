3df223238c67296ebf10668b1b421095b42adae1
NikolayPasyuk <kolyan-1812@mail.ru>
2022-06-10 21:21:07 +0300

    Task 39

--- src/lessons/lesson9/questions_9.3.js
+++ src/lessons/lesson9/questions_9.3.js
@@ -1,6 +1,17 @@
 //Task 38
 
+/*
 console.log('1' + 2)
 console.log(2 + '1')
 console.log(2 - '1')
-console.log([1, 2, 3] + {})
+console.log([1, 2, 3] + {})*/
+
+
+//Task 39
+
+Object.prototype.toString = function () {
+    return 'custom string representation'
+}
+
+const a = Object.create(null, {})
+console.log(a + '')
