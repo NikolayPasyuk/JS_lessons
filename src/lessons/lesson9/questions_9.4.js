7567a3e07e4e539a156dc53362c0d04772450294
NikolayPasyuk <kolyan-1812@mail.ru>
2022-07-27 18:37:49 +0300

    Find the maximum number in an array

--- src/lessons/lesson9/questions_9.4.js
+++ src/lessons/lesson9/questions_9.4.js
@@ -9,3 +9,16 @@ for (let i = 0; i < numbers.length; i++) {
     }
 }
 console.log(minValue)*/
+
+
+//В массиве найти максимальное число
+
+const numbers = [1, 34, 45, -123, 1234, 23]
+let maxValue = [0]
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxValue) {
+        maxValue = numbers[i]
+    }
+}
+console.log(maxValue)
