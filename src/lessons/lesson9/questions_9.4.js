66801ad2039ec0112cec240de4acd561de92cdaf
NikolayPasyuk <kolyan-1812@mail.ru>
2022-07-27 18:32:17 +0300

    Find the minimum number in an array

--- src/lessons/lesson9/questions_9.4.js
+++ src/lessons/lesson9/questions_9.4.js
@@ -1,1 +1,11 @@
-//Task 58
+//В массиве найти минимальное число
+
+/*const numbers = [150, 45, 66, 2, 4, 6, -2, -10, -100, 100]
+let minValue = numbers[0]
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < minValue) {
+        minValue = numbers[i]
+    }
+}
+console.log(minValue)*/
