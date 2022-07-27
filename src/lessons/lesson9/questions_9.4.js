4c47a1b3cd89524ac1c050a8aae01f81461304d9
NikolayPasyuk <kolyan-1812@mail.ru>
2022-07-27 19:34:14 +0300

    bottom-up dynamic programming

--- src/lessons/lesson9/questions_9.4.js
+++ src/lessons/lesson9/questions_9.4.js
@@ -44,9 +44,25 @@ alert(factorial(5))*/
 
 //Напишите функцию fib(n) которая возвращает n-е число Фибоначчи
 
+/*
 function fib(n) {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2)
 }
 
 alert(fib(3))
-alert(fib(7))
+alert(fib(7))*/
+
+// динамическое программирование снизу вверх
+
+function fib(n) {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+alert(fib(3))
