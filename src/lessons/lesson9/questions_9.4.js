bcedb26e66c06a49280fe4d9f54847f5447f2a49
NikolayPasyuk <kolyan-1812@mail.ru>
2022-07-27 19:01:58 +0300

    The basis of the recursion is the value 0

--- src/lessons/lesson9/questions_9.4.js
+++ src/lessons/lesson9/questions_9.4.js
@@ -27,8 +27,15 @@ console.log(maxValue)*/
 
 //написать функцию factorial(n), которая возвращает n!, используя рекурсию
 
+/*
 function factorial(n) {
     return (n !== 1) ? n * factorial(n - 1) : 1
+}
+
+alert(factorial(5))*/
+
+function factorial(n) {
+    return n ? n * factorial(n - 1) : 1
 }
 
 alert(factorial(5))
