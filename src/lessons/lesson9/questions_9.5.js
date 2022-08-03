de138764f45968da9868aa1c3728c26af6895256
NikolayPasyuk <kolyan-1812@mail.ru>
2022-08-03 21:17:12 +0300

    what will the function foo and bar output

--- src/lessons/lesson9/questions_9.5.js
+++ src/lessons/lesson9/questions_9.5.js
@@ -1,5 +1,6 @@
 //what will the console log output
 
+/*
 {
     var a = 10
 }
@@ -10,6 +11,20 @@ console.log(b)
     var b = 10
 }
 {
-    var c = 10
+    let c = 10
 }
-console.log(c)
+console.log(c)*/
+
+
+foo()
+
+function foo() {
+    console.log('foo')
+}
+
+bar()
+{
+    function bar() {
+        console.log('bar')
+    }
+}
