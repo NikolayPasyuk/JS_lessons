841f6d616f4e3f741927ef53fc37a3f44c401dae
NikolayPasyuk <kolyan-1812@mail.ru>
2022-08-03 21:56:13 +0300

    Task 61

--- src/lessons/lesson9/questions_9.5.js
+++ src/lessons/lesson9/questions_9.5.js
@@ -53,10 +53,29 @@ console.log(a)*/
 
 
 //Task 60
+/*
 for (var i = 0; i < 10; i++) {
 }
 console.log(i)
 
 for (let j = 0; j < 10; j++) {
 }
-console.log(j)
+console.log(j)*/
+
+
+//Task 61
+function Counter() {
+    let count = 0
+
+    this.up = function () {
+        return ++count
+    }
+    this.down = function () {
+        return --count
+    }
+}
+
+let counter = new Counter()
+console.log(counter.up())
+console.log(counter.up())
+console.log(counter.down())
