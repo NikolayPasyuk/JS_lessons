8dff95afde625b15f34e33b31cde5da76bf394e7
NikolayPasyuk <kolyan-1812@mail.ru>
2022-08-14 12:41:27 +0300

    Primitives

--- src/lessons/lesson9/questions_9.6.js
+++ src/lessons/lesson9/questions_9.6.js
@@ -19,3 +19,12 @@ function accum(string) {
 
 /*const accum = (string) =>
     string.toUpperCase().split('').map((el, i) => `${el}${el.repeat(i).toLowerCase()}`).join('-')*/
+
+
+// Primitives
+
+const a = 10;
+const b = 20;
+const c = 10;
+console.log(a === b);
+console.log(a === c);
