e962978d850dca8def2ec2b2fa7b199021d47966
NikolayPasyuk <kolyan-1812@mail.ru>
2022-08-18 20:53:15 +0300

    Task 72

--- src/lessons/lesson9/questions_9.7.js
+++ src/lessons/lesson9/questions_9.7.js
@@ -1,5 +1,6 @@
 //Task 71
 
+/*
 function foo() {
     bar()
     {
@@ -9,4 +10,19 @@ function foo() {
     }
 }
 
-foo()
+foo()*/
+
+
+//Task 72
+
+let a = () => {
+    console.log('a')
+}
+
+const run = (cb) => {
+    setTimeout(cb, 1000)
+}
+run(a)
+a = () => {
+    console.log('new a')
+}
