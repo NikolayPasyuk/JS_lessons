3205b6093a6b97523fa0df62744e2e88af324748
NikolayPasyuk <kolyan-1812@mail.ru>
2022-09-04 20:41:52 +0300

    method Object create

--- src/lessons/lesson10/lesson10.js
+++ src/lessons/lesson10/lesson10.js
@@ -1,9 +1,27 @@
 //descriptors, iterators, objects to primitives
 
 //Object.getOwnPropertyDescriptor
+//Object.getOwnPropertyDescriptors
+//Object.create
 
-const user = {
+/*const user = {
     name: 'Alex',
     age: 23
 }
-console.log(Object.getOwnPropertyDescriptors(user))
+console.log(Object.getOwnPropertyDescriptor(user, 'name'))
+console.log(Object.getOwnPropertyDescriptor(user, 'age'))
+console.log(Object.getOwnPropertyDescriptors(user))*/
+
+
+const user = {
+    name: 'Alex'
+}
+
+const user1 = Object.create(Object.prototype, {
+    name: {
+        value: 'Alex',
+        writable: true,
+        enumerable: true,
+        configurable: true
+    }
+})
