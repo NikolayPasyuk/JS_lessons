30c82f344587e34d55db1e2c2d993273eebcc543
NikolayPasyuk <kolyan-1812@mail.ru>
2022-09-04 21:41:13 +0300

    descriptor configurable

--- src/lessons/lesson10/lesson10.js
+++ src/lessons/lesson10/lesson10.js
@@ -66,17 +66,71 @@ console.log(Object.getOwnPropertyDescriptors(user))*/
 
 
 //ENUMERABLE
-const user = Object.create(Object.prototype, {
-    name: {
-        value: 'Alex',
-        writable: true,
-        enumerable: false,
-        configurable: true
-    }
-})
+// const user = Object.create(Object.prototype, {
+//     name: {
+//         value: 'Alex',
+//         writable: true,
+//         enumerable: false,
+//         configurable: true
+//     }
+// })
+//
+// console.log(Object.keys(user))
+//
+// for (let k in user) {
+//     console.log(k)
+// }
 
-console.log(Object.keys(user))
 
-for (let k in user) {
-    console.log(k)
-}
+//CONFIGURABLE
+
+
+//1. Нельзя удалить свойство
+// 'use strict'
+// const user = Object.create(Object.prototype, {
+//     name: {
+//         value: 'Alex',
+//         writable: true,
+//         enumerable: true,
+//         configurable: false
+//     }
+// })
+//
+// delete user.name
+// console.log(user)
+
+
+//2. нельзя переопределять enumerable и configurable как с true -> false так и с false -> true
+// const user = Object.create(Object.prototype, {
+//     name: {
+//         value: 'Alex',
+//         writable: true,
+//         enumerable: true,
+//         configurable: true
+//     }
+// })
+//
+// Object.defineProperty(user, 'name', {
+//     value: 'Alex',
+//     writable: true,
+//     enumerable: false,
+//     configurable: false
+// })
+
+//3. Мы не можем менять writable false -> true
+// const user = Object.create(Object.prototype, {
+//     name: {
+//         value: 'Alex',
+//         writable: false,
+//         enumerable: true,
+//         configurable: false
+//     }
+// })
+//
+// Object.defineProperty(user, 'name', {
+//     value: 'Hanna',
+//     writable: false,
+//     enumerable: true,
+//     configurable: false
+// })
+// console.log(user)
