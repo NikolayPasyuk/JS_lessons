82c71790a0e2ea214084d190fae3bf2979e9e306
NikolayPasyuk <kolyan-1812@mail.ru>
2022-09-29 15:16:19 +0300

    methods map and filter

---
+++ src/lessons/lesson11/lesson_11.2.js
@@ -0,0 +1,86 @@
+const students = [
+    {
+        id: 1,
+        name: 'Bob',
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        id: 3,
+        name: 'Nick',
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        id: 4,
+        name: 'John',
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+]
+
+
+// const getNames = (arr) => {
+//     const result = []
+//     const getNewValue = (el) => el.name
+//     for (let i = 0; i < arr.length; i++) {
+//         const newValue = getNewValue(arr[i])
+//         result[i] = newValue
+//     }
+//
+//     return result
+// }
+//
+// console.log(getNames(students))
+
+
+// const getData = (arr) => {
+//     const result = []
+//     const getNewValue = (el) => `${el.name}, ${el.age} yo, ${el.scores} scores.`
+//     for (let i = 0; i < arr.length; i++) {
+//         const newValue = getNewValue(arr[i])
+//         result[i] = newValue
+//     }
+//     return result
+// }
+// console.log(getData(students))
+
+
+const getNewArray = (arr, fn) => {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = fn(arr[i])
+        result[i] = newValue
+    }
+    return result
+}
+console.log(getNewArray(students, (el) => el.name))
+console.log(getNewArray(students, (el) => `${el.name}, ${el.age} yo, ${el.scores} scores.`))
+console.log(getNewArray(students, (el) => el.scores))
+
+console.log(students.map((el) => el.name))
+console.log(students.map((el) => `${el.name}, ${el.age} yo, ${el.scores} scores.`))
+console.log(students.map((el) => el.scores))
+
+
+const easyFilter = (arr, fn) => {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i]) === true) {
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+console.log(easyFilter(students, el => el.scores >= 100))
+console.log(students.filter(el => el.scores >= 100))
