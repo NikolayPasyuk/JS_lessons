ea38ef49b0af4f7a68afa52fd047004463036aa1
NikolayPasyuk <kolyan-1812@mail.ru>
2022-10-29 11:00:39 +0300

    find the largest number in an array, method reduce

--- src/lessons/lesson11/lesson_11.4.js
+++ src/lessons/lesson11/lesson_11.4.js
@@ -42,3 +42,6 @@ console.log(tasks[todoListId_1][2].title)
 const numbers = [1, 2, 3, 4, 5]
 
 console.log(numbers.reduce((acc, el) => acc + el, 0))
+
+
+console.log(numbers.reduce((acc, el) => acc > el ? acc : el))
