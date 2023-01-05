e1a5de39a0e9dfa376df9a0fa854f7b25454784c
NikolayPasyuk <kolyan-1812@mail.ru>
2023-01-05 16:59:14 +0300

    problems with this

--- src/lessons/lesson11/lesson_11.6.js
+++ src/lessons/lesson11/lesson_11.6.js
@@ -1,25 +1,25 @@
 //class
 
-class User {
-
-    age = 23
-
-    someFunc = () => {
-    }
-    someFunc2 = () => {
-    }
-
-    constructor() {
-        this.name = name
-    }
-
-    sayHi() {
-        console.log(this.name)
-    }
-}
-
-const user = new User('Alex')
-console.log(user)
+// class User {
+//
+//     age = 23
+//
+//     someFunc = () => {
+//     }
+//     someFunc2 = () => {
+//     }
+//
+//     constructor() {
+//         this.name = name
+//     }
+//
+//     sayHi() {
+//         console.log(this.name)
+//     }
+// }
+//
+// const user = new User('Alex')
+// console.log(user)
 
 // function User(name) {
 //     this.name = name
@@ -28,3 +28,12 @@ console.log(user)
 // User.prototype.sayHi = function () {
 //     console.log(this.name)
 // }
+
+
+// ============== NOT CLEAN SUGAR ================
+
+//[[isClassConstructor]] ---> can not call without new
+class Test {}
+
+Test()// this --> undefined --> window
+new Test()// this --> {}
