fb5d5621cc31f6ca8a2b1f7f1c7284c631ada2a4
NikolayPasyuk <kolyan-1812@mail.ru>
2023-01-17 21:45:12 +0300

    Global Scope

--- src/lessons/lesson11/lesson_11.7.js
+++ src/lessons/lesson11/lesson_11.7.js
@@ -1,15 +1,29 @@
 //prototypes
 
-const alex = {
-    name: 'Alex',
-    showName() {
-        console.log(this.name)
-    }
-}
+// const alex = {
+//     name: 'Alex',
+//     showName() {
+//         console.log(this.name)
+//     }
+// }
+//
+// const hanna = {
+//     name: 'Hanna'
+// }
+//
+// hanna.__proto__ = alex
+// hanna.showName()
 
-const hanna = {
-    name: 'Hanna'
-}
 
-hanna.__proto__ = alex
-hanna.showName()
+//this
+
+
+//1. Global Scope ---> global object(window)
+
+console.log(this)
+
+console.log(this === window)
+
+this.age = 23
+
+console.log(window.age)
