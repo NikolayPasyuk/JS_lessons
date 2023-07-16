6bd3567a719a29160ecaa6ec73e78f6c21631933
NikolayPasyuk <kolyan-1812@mail.ru>
2023-07-16 17:13:34 +0300

    Write a function to check for a prime number (by loop and recursion)

--- src/lessons/lesson12/lesson12.js
+++ src/lessons/lesson12/lesson12.js
@@ -3,3 +3,33 @@ function isPalindrome(word) {
     const reversed = word.split('').reverse().join('');
     return word === reversed;
 }
+
+
+// Написать функцию проверки на простое число (циклом и рекурсией)
+
+// Проверка простого числа с помощью цикла
+function isPrimeLoop(n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Проверка простого числа с помощью рекурсии
+function isPrimeRecursion(n, i = 2) {
+    if (n <= 1) {
+        return false;
+    }
+    if (i > Math.sqrt(n)) {
+        return true;
+    }
+    if (n % i === 0) {
+        return false;
+    }
+    return isPrimeRecursion(n, i + 1);
+}
